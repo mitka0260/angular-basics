2eba8ae5a708b907fcd8ec8b6c45fa5ae7a1211d
mitka0260 <mitkaut@gmail.com>
2021-01-06 16:35:09 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+// введём динамические данные прямо в шаблон
 export class AppComponent {
-  title = 'angular-basics';
+  title = 'Dynamic title';
+  number = 42;
+  arr = [1, 2, 3];
+  obj = {a: 1, b: {c: 2}};
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {PostComponent} from './post/post.component';
+import { Post2Component } from './post2/post2.component';
+import { Post3Component } from './post3/post3.component';
+import {Post4Component} from './post4/post4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    Post2Component,
+    Post3Component,
+    Post4Component
   ],
   imports: [
     BrowserModule
@@ -13,4 +21,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
